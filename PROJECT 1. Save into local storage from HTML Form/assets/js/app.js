c7959761f6c3392162feedeb50dd304c695b8de6
Kalenga Bwalya <kalengabwalya@gmail.com>
2019-04-30 08:56:19 +0200

    Adding new elements into the DOM

--- PROJECT 1. Save into local storage from HTML Form/assets/js/app.js
+++ PROJECT 1. Save into local storage from HTML Form/assets/js/app.js
@@ -1,5 +1,5 @@
 // Variables
-
+const tweetList = document.querySelector('#tweet-list');
 
 //Event Listeners
 eventListeners();
@@ -13,5 +13,25 @@ function eventListeners(){
 // Functions
 function newTweet(event){
      event.preventDefault();
-     console.log('form submitted');
+     
+    // Read the text area value
+    const tweet = document.querySelector('#tweet').value;
+
+    // Create the remove button
+    const removeBtn = document.createElement('a');
+    removeBtn.classList = 'remove-tweet';
+    removeBtn.textContent = 'X';
+ 
+    // Create an li element
+    let li = document.createElement('li');
+    li.textContent = tweet;
+
+    // Add the remove button to each tweet
+    li.appendChild(removeBtn);
+    
+    // Alert
+    alert('New Tweet?')
+
+    // Append tweet to the list
+    tweetList.appendChild(li);
 }
